d388cbbac8de8878de84cdcfe29366accaadf462
Alex <amurphy345@gmail.com>
2016-06-08 11:46:30 -0600

    portfolio test 3

--- public/portfolio/wp-content/themes/salient/nectar/nectar-vc-addons/vc_templates/testimonial.php
+++
@@ -1,7 +0,0 @@
-<?php 
-
-extract(shortcode_atts(array("name" => '', "quote" => ''), $atts));
-	
-echo '<blockquote><p>"'.$quote.'"</p>'. '<span>&minus; '.$name.'</span></blockquote>';
-
-?>
--- public/portfolio/wp-content/themes/salient/wpbakery/js_composer/composer/lib/shortcodes/google_maps.php
+++
@@ -1,6 +0,0 @@
-<?php
-class WPBakeryShortCode_VC_Gmaps extends WPBakeryShortCode {
-}
-
-class WPBakeryShortCode_Nectar_Gmap extends WPBakeryShortCode {
-}
